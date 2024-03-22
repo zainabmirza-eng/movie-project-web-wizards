715232bc8708349248d6573a28321d93741b97c5
Zainab <zainabmirzafatah99@gmail.com>
2024-03-22 20:13:27 +0300

    I fixed the tailwind and added movie details page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['image.tmdb.org'], // Configure the allowed domains for images
+    },
+  };
+  
+  export default nextConfig;
+  
