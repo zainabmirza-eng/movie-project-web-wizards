faed3cfbb781fa9f15a0cde872f7859431a803ca
Omer Sardar <omar.khoshnaw@gmail.com>
2024-03-25 21:10:11 +0300

    Added Footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-zinc-900 text-white pb-4 pt-8">
+    {/* <img src="" alt="" className="w-64 md:w-80" /> */}
+    <div className="max-w-7xl mx-auto px-12 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        {/* Ali Izzadin */}
+        <div className="bg-white p-6 rounded-lg shadow-2xl hover:shadow-cyan-900 hover:border-cyan-800 border-2 border-transparent hover:font-bold">
+            <img src="https://avatars.githubusercontent.com/u/157591718?v=4" alt="Person 1" className="w-32 h-32 mx-auto rounded-full mb-4 border-cyan-800 border-2" />
+            <h3 className="text-lg font-semibold text-cyan-950 text-center ">Ali Izz-aldin</h3>
+            <div className="flex justify-center space-x-4 mt-2">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/108px-LinkedIn_icon_circle.svg.png" alt="" className="size-6" />
+                <a href="https://www.linkedin.com/in/ali-izz-aldin-406975298/" target="_blank" className="text-blue-900 hover:text-blue-700">LinkedIn</a>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/300px-GitHub_Invertocat_Logo.svg.png" alt="" className="size-6 mx-0" />
+                <a href="https://github.com/r0deo" target="_blank" className="text-black hover:text-gray-700">GitHub</a>
+            </div>
+        </div>
+        {/* Halwest */}
+        <div className="bg-white p-6 rounded-lg shadow-2xl hover:shadow-cyan-900 hover:border-cyan-800 border-2 border-transparent hover:font-bold">
+            <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="Person 1" className="w-32 h-32 mx-auto rounded-full mb-4 border-cyan-800 border-2" />
+            <h3 className="text-lg font-semibold text-cyan-950 text-center ">Halwest Abubakir</h3>
+            <div className="flex justify-center space-x-4 mt-2">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/108px-LinkedIn_icon_circle.svg.png" alt="" className="size-6" />
+                <a href="https://www.linkedin.com/in/" target="_blank" className="text-blue-900 hover:text-blue-700">LinkedIn</a>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/300px-GitHub_Invertocat_Logo.svg.png" alt="" className="size-6 mx-0" />
+                <a href="https://github.com/H4lw35t" target="_blank" className="text-black hover:text-gray-700">GitHub</a>
+            </div>
+        </div>
+        {/* omer */}
+        <div className="bg-white p-6 rounded-lg shadow-2xl hover:shadow-cyan-900 hover:border-cyan-800 border-2 border-transparent hover:font-bold">
+            <img src="https://avatars.githubusercontent.com/u/147993556?s=400&u=8c2105f3a37df0d6afb09d4892a6bb7a8921914f&v=4" alt="Person 1" className="w-32 h-32 mx-auto rounded-full mb-4 border-cyan-800 border-2" />
+            <h3 className="text-lg font-semibold text-cyan-950 text-center ">Omar Sardar</h3>
+            <div className="flex justify-center space-x-4 mt-2">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/108px-LinkedIn_icon_circle.svg.png" alt="" className="size-6" />
+                <a href="https://www.linkedin.com/in/osmo" target="_blank" className="text-blue-900 hover:text-blue-700">LinkedIn</a>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/300px-GitHub_Invertocat_Logo.svg.png" alt="" className="size-6 mx-0" />
+                <a href="https://github.com/Omar-Khoshnaw" target="_blank" className="text-black hover:text-gray-700">GitHub</a>
+            </div>
+        </div>
+        {/* Zaynab */}
+        <div className="bg-white p-6 rounded-lg shadow-2xl hover:shadow-cyan-900 hover:border-cyan-800 border-2 border-transparent hover:font-bold">
+            <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="Person 1" className="w-32 h-32 mx-auto rounded-full mb-4 border-cyan-800 border-2" />
+            <h3 className="text-lg font-semibold text-cyan-950 text-center ">Zainab Mirza</h3>
+            <div className="flex justify-center space-x-4 mt-2">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/108px-LinkedIn_icon_circle.svg.png" alt="" className="size-6" />
+                <a href="https://www.linkedin.com/in/" target="_blank" className="text-blue-900 hover:text-blue-700">LinkedIn</a>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/300px-GitHub_Invertocat_Logo.svg.png" alt="" className="size-6 mx-0" />
+                <a href="https://github.com/zainabmirza-eng" target="_blank" className="text-black hover:text-gray-700">GitHub</a>
+            </div>
+        </div>
+    </div>
+    <p className="p-0 mt-3 text-center">Copyright © 2024 CineDeLuna . All rights reserved.</p>
+</footer>
+
+  )
+}
