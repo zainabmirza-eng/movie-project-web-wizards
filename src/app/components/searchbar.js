f4c2a365fd574c2972c81dc8dd69e92e8d14cb48
Zainab <zainabmirzafatah99@gmail.com>
2024-03-29 16:02:29 +0300

    added

--- src/app/components/searchbar.js
+++ src/app/components/searchbar.js
@@ -77,7 +77,7 @@ export default function Search() {
             </div>
           </form>
         </div>
-        <ul className="flex sm:flex-row sm:justify-center justify-center p-4 md:p-0 mt-4 font-medium sm:space-x-10 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 text-sm sm:text-base">
+        <ul className="flex sm:flex-row space-x-2 sm:justify-center justify-center p-4 md:p-0 mt-4 font-medium sm:space-x-10 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 text-sm sm:text-base">
           <li>
             <Link
               href="/"
